0c7d6af519409db38757a6b5743d6d7787e4f1a6
carolrss <65054120+carolrss@users.noreply.github.com>
2023-04-03 12:43:34 -0300

    Add files via upload

---
+++ atividade6/atividade6.js
@@ -0,0 +1,23 @@
+
+
+    var nomeAluno;
+    var nota1;
+    var nota2;
+    var nota3; 
+
+
+    nomeAluno = prompt("Qual o nome do aluno:");
+    nota1 =  parseFloat(prompt("Digite a nota  1:")) ;
+    nota2 =  parseFloat(prompt("Digite a nota  2:")) ;
+    nota3 =  parseFloat(prompt("Digite a nota  3:")) ;
+
+
+    var media = (nota1 + nota3 + nota3) /3 ;
+
+    alert( "Nome do Aluno: " + nomeAluno + "\n Media do Aluno: " + media);
+
+
+
+
+
+
---
+++ atividade6/operacoes.js
@@ -0,0 +1,12 @@
+var numero1;
+var numero2;
+
+
+numero1 = parseFloat(prompt("Digite o numero 1"));
+numero2 = parseFloat(prompt("Digite o numero 2"));
+
+alert("soma = " + (numero1 + numero2));
+alert("\n Subtrção = " + (numero1 - numero2));
+alert("\n produto = " + (numero1 * numero2));
+alert("\n Divisao = " + (numero1 / numero2));
+alert("\n Resto da divisao = " + (numero1 % numero2));
